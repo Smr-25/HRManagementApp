de77dcf2ee5f2667267b2bd3221afe9d1af52064
Smr-25 <samiraa@code.edu.az>
2025-10-17 23:46:26 +0400

    17.10.25 project task v2

--- HRManagementApp/Files/FileGuider.cs
+++ HRManagementApp/Files/FileGuider.cs
@@ -1,55 +1,47 @@
 ﻿using HRManagementApp.Models;
 using Newtonsoft.Json;
-using System.Text;
 
 namespace HRManagementApp.Files
 {
     public static class FileGuider
     {
-        // Use a relative path so the app works across OSes and environments
-        public static string Path = System.IO.Path.Combine("Datas", "Departments", "Departments.json");
+        public static string path = "C:\\Users\\samiraa\\Desktop\\MiniApp\\HRManagementApp\\Datas\\Departments\\Departments.json";
+        //public void CreateDirectory()
+        //{
+        //    if (!Directory.Exists(path))
+        //    {
+                
+        //    }
+        //}
+        //public void CreateFile(ref string path)
+        //{
+        //    if (!File.Exists(path))
+        //    {
+        //        path += "";
+        //        File.Create(path);
+        //    }
 
-        private static void EnsureFileExists()
-        {
-            var dir = System.IO.Path.GetDirectoryName(Path) ?? "";
-            if (!Directory.Exists(dir))
-            {
-                Directory.CreateDirectory(dir);
-            }
-
-            if (!File.Exists(Path))
-            {
-                // create empty JSON array
-                File.WriteAllText(Path, "[]", Encoding.UTF8);
-            }
-        }
-
+        //}
+        
         public static List<Department> ReadJsonFile()
         {
-            EnsureFileExists();
-
-            using FileStream fileStream = new FileStream(Path, FileMode.Open, FileAccess.Read, FileShare.Read);
+            using FileStream fileStream = new FileStream(path,FileMode.Open);
             using StreamReader streamReader = new StreamReader(fileStream);
             string datas = streamReader.ReadToEnd();
-
-            var departments = JsonConvert.DeserializeObject<List<Department>>(datas) ?? new List<Department>();
-
-            // Ensure Employees lists are not null after deserialization
-            foreach (var d in departments)
+            
+            List<Department> departments = JsonConvert.DeserializeObject<List<Department>>(datas);
+            if(departments == null)
             {
-                if (d.Employees == null)
-                    d.Employees = new List<Employee>();
+                departments = new();
             }
-
             return departments;
         }
 
         public static void WriteJsonFile(List<Department> objects)
         {
-            EnsureFileExists();
-            using FileStream fileStream = new FileStream(Path, FileMode.Create, FileAccess.Write, FileShare.None);
+            using FileStream fileStream = new FileStream(path,FileMode.Create);
             using StreamWriter streamWriter = new StreamWriter(fileStream);
-            string datas = JsonConvert.SerializeObject(objects, Formatting.Indented);
+            string datas = JsonConvert.SerializeObject(objects);
             streamWriter.Write(datas);
         }
     }
--- HRManagementApp/obj/Debug/net8.0/HRManagementApp.AssemblyInfo.cs
+++ HRManagementApp/obj/Debug/net8.0/HRManagementApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HRManagementApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e28f921c02a8f2d71a2725d18f11db796c3891e3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2b77735f07a129dcd91da5b82bf62c16d4561226")]
 [assembly: System.Reflection.AssemblyProductAttribute("HRManagementApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HRManagementApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
