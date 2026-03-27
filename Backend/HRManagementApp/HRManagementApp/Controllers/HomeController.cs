69fd6aeae9a86f63532e1774504c87938a6822c4
Smr-25 <samiraa@code.edu.az>
2026-03-27 12:13:23 +0400

    27.03.26 commit

---
+++ Backend/HRManagementApp/HRManagementApp/Controllers/HomeController.cs
@@ -0,0 +1,19 @@
+using Microsoft.AspNetCore.Mvc;
+using HRManagementApp.Models;
+using System.Diagnostics;
+
+namespace HRManagementApp.Controllers;
+
+public class HomeController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
